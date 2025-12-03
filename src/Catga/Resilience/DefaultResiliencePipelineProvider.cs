04b8f769b7e4735a84a0f5ebcd8f5f1c45f6e851
HCricle <15218450198@163.com>
2025-12-04 07:34:20 +0800

    refactor: simplify DefaultResiliencePipelineProvider
    - DefaultResiliencePipelineProvider: 286 -> 71 lines (-75%)
    - Extract common Build() method for pipeline construction
    - Consolidate Metric() and Event() helpers
    
    All 751 unit tests passing

--- src/Catga/Resilience/DefaultResiliencePipelineProvider.cs
+++ src/Catga/Resilience/DefaultResiliencePipelineProvider.cs
@@ -16,270 +16,55 @@ public sealed class DefaultResiliencePipelineProvider : IResiliencePipelineProvi
     private readonly ResiliencePipeline _transportSend;
     private readonly ResiliencePipeline _persistence;
 
-    public DefaultResiliencePipelineProvider(CatgaResilienceOptions options)
+    public DefaultResiliencePipelineProvider(CatgaResilienceOptions? options = null)
     {
-        options ??= new CatgaResilienceOptions();
-        _mediator = BuildMediator(options);
-        _transportPublish = BuildTransport(options, ResilienceKeys.TransportPublish);
-        _transportSend = BuildTransport(options, ResilienceKeys.TransportSend);
-        _persistence = BuildPersistence(options);
+        var o = options ?? new CatgaResilienceOptions();
+        _mediator = Build(ResilienceKeys.Mediator, o.MediatorBulkheadConcurrency, o.MediatorBulkheadQueueLimit, o.MediatorTimeout, 20);
+        _transportPublish = Build(ResilienceKeys.TransportPublish, o.TransportBulkheadConcurrency, o.TransportBulkheadQueueLimit, o.TransportTimeout, 50, o.TransportRetryDelay, o.TransportRetryCount);
+        _transportSend = Build(ResilienceKeys.TransportSend, o.TransportBulkheadConcurrency, o.TransportBulkheadQueueLimit, o.TransportTimeout, 50, o.TransportRetryDelay, o.TransportRetryCount);
+        _persistence = Build(ResilienceKeys.Persistence, o.PersistenceBulkheadConcurrency, o.PersistenceBulkheadQueueLimit, o.PersistenceTimeout, 50, o.PersistenceRetryDelay, o.PersistenceRetryCount);
     }
 
-    private static ResiliencePipeline BuildMediator(CatgaResilienceOptions o)
+    private static ResiliencePipeline Build(string comp, int concurrency, int queue, TimeSpan timeout, int minThroughput, TimeSpan? retryDelay = null, int retryCount = 0)
     {
-        var builder = new ResiliencePipelineBuilder();
-        builder.AddRateLimiter(new RateLimiterStrategyOptions
-        {
-            DefaultRateLimiterOptions = new ConcurrencyLimiterOptions
+        var b = new ResiliencePipelineBuilder();
+        if (concurrency > 0)
+            b.AddRateLimiter(new RateLimiterStrategyOptions
             {
-                PermitLimit = o.MediatorBulkheadConcurrency,
-                QueueLimit = o.MediatorBulkheadQueueLimit
-            },
-            OnRejected = _ =>
-            {
-                CatgaDiagnostics.ResilienceBulkheadRejected.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Mediator));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceBulkheadRejected, ResilienceKeys.Mediator);
-                return default;
-            }
-        });
-        builder.AddCircuitBreaker(new CircuitBreakerStrategyOptions
-        {
-            FailureRatio = 0.5,
-            MinimumThroughput = 20,
-            BreakDuration = TimeSpan.FromSeconds(30),
-            OnOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitOpened.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Mediator));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitOpen, ResilienceKeys.Mediator);
-                return default;
-            },
-            OnHalfOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitHalfOpened.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Mediator));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitHalfOpen, ResilienceKeys.Mediator);
-                return default;
-            },
-            OnClosed = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitClosed.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Mediator));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitClosed, ResilienceKeys.Mediator);
-                return default;
-            }
-        });
-        builder.AddTimeout(new TimeoutStrategyOptions
-        {
-            Timeout = o.MediatorTimeout,
-            OnTimeout = _ =>
-            {
-                CatgaDiagnostics.ResilienceTimeouts.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Mediator));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceTimeout, ResilienceKeys.Mediator);
-                return default;
-            }
-        });
-        return builder.Build();
-    }
-
-    private static ResiliencePipeline BuildTransport(CatgaResilienceOptions o, string component)
-    {
-        var builder = new ResiliencePipelineBuilder();
-        builder.AddRateLimiter(new RateLimiterStrategyOptions
-        {
-            DefaultRateLimiterOptions = new ConcurrencyLimiterOptions
-            {
-                PermitLimit = o.TransportBulkheadConcurrency,
-                QueueLimit = o.TransportBulkheadQueueLimit
-            },
-            OnRejected = _ =>
-            {
-                CatgaDiagnostics.ResilienceBulkheadRejected.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceBulkheadRejected, component);
-                return default;
-            }
-        });
-        builder.AddCircuitBreaker(new CircuitBreakerStrategyOptions
-        {
-            FailureRatio = 0.5,
-            MinimumThroughput = 50,
-            BreakDuration = TimeSpan.FromSeconds(30),
-            OnOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitOpened.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitOpen, component);
-                return default;
-            },
-            OnHalfOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitHalfOpened.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitHalfOpen, component);
-                return default;
-            },
-            OnClosed = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitClosed.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitClosed, component);
-                return default;
-            }
-        });
-        builder.AddTimeout(new TimeoutStrategyOptions
-        {
-            Timeout = o.TransportTimeout,
-            OnTimeout = _ =>
-            {
-                CatgaDiagnostics.ResilienceTimeouts.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceTimeout, component);
-                return default;
-            }
-        });
-        builder.AddRetry(new RetryStrategyOptions
-        {
-            Delay = o.TransportRetryDelay,
-            UseJitter = true,
-            BackoffType = DelayBackoffType.Exponential,
-            MaxRetryAttempts = o.TransportRetryCount,
-            ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>().Handle<Exception>(),
-            OnRetry = args =>
-            {
-                CatgaDiagnostics.ResilienceRetries.Add(1, new KeyValuePair<string, object?>("component", component));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceRetry, component, args.AttemptNumber);
-                return default;
-            }
-        });
-        return builder.Build();
-    }
-
-    private static ResiliencePipeline BuildPersistence(CatgaResilienceOptions o)
-    {
-        var builder = new ResiliencePipelineBuilder();
-        if (o.PersistenceBulkheadConcurrency > 0)
-        {
-            builder.AddRateLimiter(new RateLimiterStrategyOptions
-            {
-                DefaultRateLimiterOptions = new ConcurrencyLimiterOptions
-                {
-                    PermitLimit = o.PersistenceBulkheadConcurrency,
-                    QueueLimit = o.PersistenceBulkheadQueueLimit
-                },
-                OnRejected = _ =>
-                {
-                    CatgaDiagnostics.ResilienceBulkheadRejected.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Persistence));
-                    AddResilienceEvent(CatgaActivitySource.Events.ResilienceBulkheadRejected, ResilienceKeys.Persistence);
-                    return default;
-                }
+                DefaultRateLimiterOptions = new ConcurrencyLimiterOptions { PermitLimit = concurrency, QueueLimit = queue },
+                OnRejected = _ => { Metric(CatgaDiagnostics.ResilienceBulkheadRejected, comp); Event(CatgaActivitySource.Events.ResilienceBulkheadRejected, comp); return default; }
             });
-        }
-        builder.AddCircuitBreaker(new CircuitBreakerStrategyOptions
+        b.AddCircuitBreaker(new CircuitBreakerStrategyOptions
         {
-            FailureRatio = 0.5,
-            MinimumThroughput = 50,
-            BreakDuration = TimeSpan.FromSeconds(30),
-            OnOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitOpened.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Persistence));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitOpen, ResilienceKeys.Persistence);
-                return default;
-            },
-            OnHalfOpened = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitHalfOpened.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Persistence));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitHalfOpen, ResilienceKeys.Persistence);
-                return default;
-            },
-            OnClosed = _ =>
-            {
-                CatgaDiagnostics.ResilienceCircuitClosed.Add(1, new KeyValuePair<string, object?>("component", ResilienceKeys.Persistence));
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceCircuitClosed, ResilienceKeys.Persistence);
-                return default;
-            }
+            FailureRatio = 0.5, MinimumThroughput = minThroughput, BreakDuration = TimeSpan.FromSeconds(30),
+            OnOpened = _ => { Metric(CatgaDiagnostics.ResilienceCircuitOpened, comp); Event(CatgaActivitySource.Events.ResilienceCircuitOpen, comp); return default; },
+            OnHalfOpened = _ => { Metric(CatgaDiagnostics.ResilienceCircuitHalfOpened, comp); Event(CatgaActivitySource.Events.ResilienceCircuitHalfOpen, comp); return default; },
+            OnClosed = _ => { Metric(CatgaDiagnostics.ResilienceCircuitClosed, comp); Event(CatgaActivitySource.Events.ResilienceCircuitClosed, comp); return default; }
         });
-        builder.AddTimeout(new TimeoutStrategyOptions
+        b.AddTimeout(new TimeoutStrategyOptions
         {
-            Timeout = o.PersistenceTimeout,
-            OnTimeout = _ =>
-            {
-                CatgaDiagnostics.ResilienceTimeouts.Add(1);
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceTimeout, ResilienceKeys.Persistence);
-                return default;
-            }
+            Timeout = timeout,
+            OnTimeout = _ => { Metric(CatgaDiagnostics.ResilienceTimeouts, comp); Event(CatgaActivitySource.Events.ResilienceTimeout, comp); return default; }
         });
-        builder.AddRetry(new RetryStrategyOptions
-        {
-            Delay = o.PersistenceRetryDelay,
-            UseJitter = true,
-            BackoffType = DelayBackoffType.Exponential,
-            MaxRetryAttempts = o.PersistenceRetryCount,
-            ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>().Handle<Exception>(),
-            OnRetry = args =>
+        if (retryDelay.HasValue && retryCount > 0)
+            b.AddRetry(new RetryStrategyOptions
             {
-                CatgaDiagnostics.ResilienceRetries.Add(1);
-                AddResilienceEvent(CatgaActivitySource.Events.ResilienceRetry, ResilienceKeys.Persistence, args.AttemptNumber);
-                return default;
-            }
-        });
-        return builder.Build();
+                Delay = retryDelay.Value, UseJitter = true, BackoffType = DelayBackoffType.Exponential, MaxRetryAttempts = retryCount,
+                ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>().Handle<Exception>(),
+                OnRetry = args => { Metric(CatgaDiagnostics.ResilienceRetries, comp); Event(CatgaActivitySource.Events.ResilienceRetry, comp, args.AttemptNumber); return default; }
+            });
+        return b.Build();
     }
 
-    public async ValueTask<T> ExecuteMediatorAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken cancellationToken)
-    {
-        return await _mediator.ExecuteAsync(async ct => await action(ct), cancellationToken);
-    }
+    private static void Metric(System.Diagnostics.Metrics.Counter<long> counter, string comp) => counter.Add(1, new KeyValuePair<string, object?>("component", comp));
+    private static void Event(string name, string comp, int? attempt = null) => Activity.Current?.AddEvent(new ActivityEvent(name, tags: attempt.HasValue ? new ActivityTagsCollection { ["component"] = comp, ["attempt"] = attempt.Value } : new ActivityTagsCollection { ["component"] = comp }));
 
-    public async ValueTask ExecuteMediatorAsync(Func<CancellationToken, ValueTask> action, CancellationToken cancellationToken)
-    {
-        await _mediator.ExecuteAsync(async ct =>
-        {
-            await action(ct);
-            return 0;
-        }, cancellationToken);
-    }
-
-    public async ValueTask<T> ExecuteTransportPublishAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken cancellationToken)
-    {
-        return await _transportPublish.ExecuteAsync(async ct => await action(ct), cancellationToken);
-    }
-
-    public async ValueTask ExecuteTransportPublishAsync(Func<CancellationToken, ValueTask> action, CancellationToken cancellationToken)
-    {
-        await _transportPublish.ExecuteAsync(async ct =>
-        {
-            await action(ct);
-            return 0;
-        }, cancellationToken);
-    }
-
-    public async ValueTask<T> ExecuteTransportSendAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken cancellationToken)
-    {
-        return await _transportSend.ExecuteAsync(async ct => await action(ct), cancellationToken);
-    }
-
-    public async ValueTask ExecuteTransportSendAsync(Func<CancellationToken, ValueTask> action, CancellationToken cancellationToken)
-    {
-        await _transportSend.ExecuteAsync(async ct =>
-        {
-            await action(ct);
-            return 0;
-        }, cancellationToken);
-    }
-
-    public async ValueTask<T> ExecutePersistenceAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken cancellationToken) => await _persistence.ExecuteAsync(async ct => await action(ct), cancellationToken);
-
-    public async ValueTask ExecutePersistenceAsync(Func<CancellationToken, ValueTask> action, CancellationToken cancellationToken) => await _persistence.ExecuteAsync(async ct => { await action(ct); return 0; }, cancellationToken);
-
-    private static void AddResilienceEvent(string eventName, string component, int? attempt = null)
-    {
-        var current = Activity.Current;
-        if (current is null) return;
-
-        if (attempt.HasValue)
-        {
-            current.AddEvent(new ActivityEvent(
-                eventName,
-                tags: new ActivityTagsCollection { ["component"] = component, ["attempt"] = attempt.Value }));
-        }
-        else
-        {
-            current.AddEvent(new ActivityEvent(
-                eventName,
-                tags: new ActivityTagsCollection { ["component"] = component }));
-        }
-    }
+    public async ValueTask<T> ExecuteMediatorAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken ct) => await _mediator.ExecuteAsync(async c => await action(c), ct);
+    public async ValueTask ExecuteMediatorAsync(Func<CancellationToken, ValueTask> action, CancellationToken ct) => await _mediator.ExecuteAsync(async c => { await action(c); return 0; }, ct);
+    public async ValueTask<T> ExecuteTransportPublishAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken ct) => await _transportPublish.ExecuteAsync(async c => await action(c), ct);
+    public async ValueTask ExecuteTransportPublishAsync(Func<CancellationToken, ValueTask> action, CancellationToken ct) => await _transportPublish.ExecuteAsync(async c => { await action(c); return 0; }, ct);
+    public async ValueTask<T> ExecuteTransportSendAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken ct) => await _transportSend.ExecuteAsync(async c => await action(c), ct);
+    public async ValueTask ExecuteTransportSendAsync(Func<CancellationToken, ValueTask> action, CancellationToken ct) => await _transportSend.ExecuteAsync(async c => { await action(c); return 0; }, ct);
+    public async ValueTask<T> ExecutePersistenceAsync<T>(Func<CancellationToken, ValueTask<T>> action, CancellationToken ct) => await _persistence.ExecuteAsync(async c => await action(c), ct);
+    public async ValueTask ExecutePersistenceAsync(Func<CancellationToken, ValueTask> action, CancellationToken ct) => await _persistence.ExecuteAsync(async c => { await action(c); return 0; }, ct);
 }
